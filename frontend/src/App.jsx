afc5974f606dfbba31a38d0ad30b4cc8a82babc3
Ayush Bohra <codefingers809@gmail.com>
2024-12-17 20:45:33 +0530

    base

---
+++ frontend/src/App.jsx
@@ -0,0 +1,22 @@
+import Faq from "./Pages/Faq";
+import Landing from "./Pages/Landing";
+import Prizes from "./Pages/Prizes";
+import Sponsors from "./Pages/Sponsors";
+import Timeline from "./Pages/Timeline";
+import Tracks from "./Pages/Tracks";
+
+function App() {
+  return (
+    <>
+      <Landing />
+      <Tracks />
+      <Timeline />
+      <Prizes />
+      <Sponsors />
+      <Faq />
+    </>
+  );
+}
+
+export default App;
+
