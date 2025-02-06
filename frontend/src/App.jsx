114180709340e529d80eb4935259339dfad51331
Dhairyash Jain <jaindhairyashj@gmail.com>
2025-02-06 20:27:24 +0530

    round 1 winners added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,6 +14,7 @@ import Sponsors from "./Pages/Sponsor/Sponsor.jsx";
 import ParticleEffect from "./Pages/Tracks/ParticleEffect.jsx";
 import RegistrationBanner from "./Pages/Landing/RegDetail.jsx";
 import CommitteeSection from "./Pages/Teams/Comittee.jsx";
+import RoundOneResults from "./Pages/RoundOneResults.jsx";
 
 function App() {
   const [dimensions, setDimensions] = useState({ width: 0, height: 0 });
@@ -104,6 +105,7 @@ function App() {
       <RegistrationBanner />
       <Tracks />
       <Timeline />
+      <RoundOneResults/>
       <Sponsors />
       <Prizes />
       <Teams />
